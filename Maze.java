6d66ef39a8e19ff10f824120ff435cc6d0ae9836
tlfolkman <44623133+tlfolkman@users.noreply.github.com>
2020-01-08 20:28:54 -0700

    Maze Class

---
+++ Maze.java
@@ -0,0 +1,17 @@
+package com.example.testmaze1;
+
+// Hold the data in the JSON file
+public class Maze {
+    public String [][]array1;
+    public int rows1;
+    public int cols1;
+    public String [][]array2;
+    public int rows2;
+    public int cols2;
+    public String [][]array3;
+    public int rows3;
+    public int cols3;
+    public String [][]array4;
+    public int rows4;
+    public int cols4;
+}
